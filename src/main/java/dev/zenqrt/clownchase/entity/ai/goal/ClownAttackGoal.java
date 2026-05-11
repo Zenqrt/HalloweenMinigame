9a39cec9a13e6db7917f4fa4cf3277d038dcf87c
Walmqrt <46291857+Zenqrt@users.noreply.github.com>
2026-05-11 13:02:26 -0400

    Fix clown attacks not being in 2s intervals

--- src/main/java/dev/zenqrt/clownchase/entity/ai/goal/ClownAttackGoal.java
+++ src/main/java/dev/zenqrt/clownchase/entity/ai/goal/ClownAttackGoal.java
@@ -1,16 +1,27 @@
 package dev.zenqrt.clownchase.entity.ai.goal;
 
 import dev.zenqrt.clownchase.entity.Clown;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import org.jetbrains.annotations.NotNull;
 
 public final class ClownAttackGoal extends MeleeAttackGoal {
+
+    private int lastHitTick;
 
     public ClownAttackGoal(Clown mob, double speedModifier, boolean followingTargetEvenIfNotSeen) {
         super(mob, speedModifier, followingTargetEvenIfNotSeen);
     }
 
     @Override
-    protected int getAttackInterval() {
-        return 40;
+    protected void checkAndPerformAttack(@NotNull LivingEntity target) {
+        if (canPerformAttack(target) && mob.tickCount - lastHitTick >= 40) {
+            lastHitTick = mob.tickCount;
+
+            resetAttackCooldown();
+            mob.swing(InteractionHand.MAIN_HAND);
+            mob.doHurtTarget(getServerLevel(mob), target);
+        }
     }
 }
