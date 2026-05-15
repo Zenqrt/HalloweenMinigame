274b6dfe273dc451c0e6943eaa0fcc39814ce646
Walmqrt <46291857+Zenqrt@users.noreply.github.com>
2026-05-15 00:16:04 -0400

    Fix lightning ambience not showing all the time if too far

--- src/main/java/dev/zenqrt/clownchase/map/ambience/LightningAmbience.java
+++ src/main/java/dev/zenqrt/clownchase/map/ambience/LightningAmbience.java
@@ -2,7 +2,15 @@ package dev.zenqrt.clownchase.map.ambience;
 
 import com.google.gson.JsonObject;
 import dev.zenqrt.clownchase.game.ClownChaseGame;
-import io.papermc.paper.math.Position;
+import dev.zenqrt.clownchase.game.ClownChasePlayer;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.phys.Vec3;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -14,7 +22,6 @@ public final class LightningAmbience implements Ambience {
         return new LightningAmbience(spawnChance);
     }
 
-    private static final Position position = Position.block(0, 200, 0);
     private final float spawnChance;
 
     public LightningAmbience(float spawnChance) {
@@ -29,6 +36,27 @@ public final class LightningAmbience implements Ambience {
         if (roll > spawnChance)
             return;
 
-        game.getGameWorld().strikeLightningEffect(position.offset(0, -100, 0).toLocation(game.getGameWorld()));
+        LightningBolt lightningBolt = new LightningBolt(EntityType.LIGHTNING_BOLT, ((CraftWorld) game.getGameWorld()).getHandle());
+        lightningBolt.isEffect = true;
+
+        for (ClownChasePlayer gamePlayer : game.getPlayers().values()) {
+            Player player = gamePlayer.validatePlayer();
+            ServerPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
+
+            ClientboundAddEntityPacket addEntityPacket = new ClientboundAddEntityPacket(
+                    lightningBolt.getId(),
+                    lightningBolt.getUUID(),
+                    player.getX(),
+                    player.getY() + 150,
+                    player.getZ(),
+                    lightningBolt.getXRot(),
+                    lightningBolt.getYRot(),
+                    EntityType.LIGHTNING_BOLT,
+                    0,
+                    Vec3.ZERO,
+                    lightningBolt.getYHeadRot()
+            );
+            nmsPlayer.connection.send(addEntityPacket);
+        }
     }
 }
